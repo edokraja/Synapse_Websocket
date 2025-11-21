d52b22f95c616b5b52de0f3441a757026741eba2
edokraja <edokraja14@gmail.com>
2025-11-21 20:07:49 +0100

    Added docstrings

--- test_cases/test_endtoend.py
+++ test_cases/test_endtoend.py
@@ -6,6 +6,11 @@ from synapse_ws.websocket_server import main
 
 @pytest.fixture(scope="module", autouse=True)
 def start_server():
+    """
+    Function to start the server for the whole module and
+    before every test case, run the tests while server is running
+    and then stop.
+    """
     # create a loop to not block the server and let the test run
     new_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(new_loop)
@@ -19,5 +24,10 @@ def start_server():
 # actual test
 @pytest.mark.asyncio
 async def test_client_to_server():
+    """
+    This function is the actual test case that will be executed.
+    It will check if the client-server communication is working
+    by sending a message and checking the response.
+    """
     result = await send_message(6, 5)
     assert result == 30
